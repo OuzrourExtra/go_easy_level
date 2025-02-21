0439211a630bc1dadb9c1402fe5dc2ef0a3fbd4c
Ilyas Ouzrour <ilyas.ouzrour@gmail.com>
2025-02-21 20:59:21 +0100

    print constant

---
+++ 3_print_const/go.mod
@@ -0,0 +1,3 @@
+module github.com/ouzrourextra/GO_EASY_LEVEL/3_print_const
+
+go 1.23.5
---
+++ 3_print_const/main.go
@@ -0,0 +1,17 @@
+/*
+Project created for training
+
+By:Ouzrour Ilyas
+Date: 21/2/25
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+    const intVar int = 2
+    const stringVar string = "is a number !"
+
+    fmt.Println(intVar , stringVar)
+}
